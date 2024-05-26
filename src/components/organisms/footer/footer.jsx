e4c2b650a9a4ff3392b38adc463a146c51d5e69e
Charly Duque <charly.d.duque@gmail.com>
2024-05-26 17:23:39 -0600

    Agrego iconos Footer en una sola row

--- src/components/organisms/footer/footer.jsx
+++ src/components/organisms/footer/footer.jsx
@@ -2,8 +2,10 @@ import React from "react"
 import { StyledFooter, StyledRigthConteiner, StyledLeftConteiner, StyledContact, StyledSocialContainer } from "./footer.styles"
 import { Col, Container, Row } from "react-awesome-styled-grid"
 import Image from "../../atoms/media/image/image"
-import imgRuedas from "../../../../static/assets/images/common/logo_ruedas.png"
-import imgBCJB from "../../../../static/assets/images/common/logo_bcjb.png"
+import imgRuedas from "../../../../static/assets/images/common/Logo_Ruedas_Rebeldes2.png"
+import imgICFJ from "../../../../static/assets/images/common/Logo_ICFJ.png"
+import imgUSAID from "../../../../static/assets/images/common/Logo_USAID.png"
+import imgBCJB from "../../../../static/assets/images/common/Logo_Border_Hub_2.png"
 import ProjectIcon from "../../atoms/icons/icon/projectIcon/projectIcon"
 
 const componentId = `o-footer`
@@ -13,36 +15,47 @@ const Footer = ({ className }) => {
     <StyledFooter className={`${componentId} ${className}`}>
       <Container className={`${componentId}__container`}>
         <Row>
-          <Col className={`${componentId}__left-block`} xs={2} sm={4} md={5} lg={5} xl={5} offset={{ xs: 0, sm: 0, md: 1, lg: 1, xl: 1 }}>
-            <StyledLeftConteiner>
-              <a href="https://www.ruedasrebeldes.com/" target="_blank" rel="norefferer">
-                <Image className={`${componentId}__img-ruedas`} sourceUrl={imgRuedas} width="100%" />
-              </a>
-              <StyledContact>
-                <p>contacto@ruedasrebeldes.com Ruedas Rebeldes. Periodismo en bicicleta. Saltillo, Coahuila. MX</p>
-              </StyledContact>
-            </StyledLeftConteiner>
+          <Col xs={2} sm={4} md={5} lg={5} xl={5}>
+            <Image className={`${componentId}__img`} sourceUrl={imgRuedas} />
           </Col>
-          <Col className={`${componentId}__right-block`} xs={2} sm={4} md={5} lg={5} xl={5} offset={{ xs: 0, sm: 0, md: 0, lg: 0, xl: 0 }}>
-            <StyledRigthConteiner>
-              <Image className={`${componentId}__img-bcjb`} sourceUrl={imgBCJB} width="100%" />
-              <StyledSocialContainer>
-                <a href="https://www.facebook.com/profile.php?id=100083014976811" target="_blank" rel="norefferer">
-                  <ProjectIcon id="fb" />
-                </a>
-                <a href="https://www.tiktok.com/@ruedasrebeldes" target="_blank" rel="norefferer">
-                  <ProjectIcon id="tiktok" />
-                </a>
-                <a href="https://twitter.com/ruedasrebeldes" target="_blank" rel="norefferer">
-                  <ProjectIcon id="twitter" />
-                </a>
-                <a href="https://www.youtube.com/@ruedasrebeldes" target="_blank" rel="norefferer">
-                  <ProjectIcon id="youtube" />
-                </a>
-              </StyledSocialContainer>
-            </StyledRigthConteiner>
+          <Col xs={2} sm={4} md={5} lg={5} xl={5}>
+            <Image className={`${componentId}__img`} sourceUrl={imgBCJB} />
+          </Col>
+          <Col xs={2} sm={4} md={5} lg={5} xl={5}>
+            <Image className={`${componentId}__img`} sourceUrl={imgUSAID} />
+          </Col>
+          <Col xs={2} sm={4} md={5} lg={5} xl={5}>
+            <Image className={`${componentId}__img`} sourceUrl={imgICFJ} />
           </Col>
         </Row>
+        <Row>
+          <Col xs={2} sm={4} md={5} lg={5} xl={5}>
+            <StyledContact>
+              <p>contacto@ruedasrebeldes.com Ruedas Rebeldes. Periodismo en bicicleta. Saltillo, Coahuila. MX</p>
+
+            </StyledContact>
+          </Col>
+          <Col xs={2} sm={4} md={5} lg={5} xl={5}>
+            <StyledSocialContainer>
+              <a href="https://www.facebook.com/profile.php?id=100083014976811" target="_blank" rel="norefferer">
+                <ProjectIcon id="fb" />
+              </a>
+              <a href="https://www.tiktok.com/@ruedasrebeldes" target="_blank" rel="norefferer">
+                <ProjectIcon id="tiktok" />
+              </a>
+              <a href="https://twitter.com/ruedasrebeldes" target="_blank" rel="norefferer">
+                <ProjectIcon id="twitter" />
+              </a>
+              <a href="https://www.youtube.com/@ruedasrebeldes" target="_blank" rel="norefferer">
+                <ProjectIcon id="youtube" />
+              </a>
+            </StyledSocialContainer>
+          </Col>
+        </Row>
+        <Container>
+          <p>Monitor de movilidad Saltillo es un proyecto desarrollado con el apoyo del Hub de Periodismo de Investigación de la Frontera Norte (Border Hub), el centro internacional para Periodistas (ICFJ) y la Agencia Internacional para el Desarrollo de Estados Unidos(USAID)</p>
+
+        </Container>
       </Container>
     </StyledFooter>
   )
