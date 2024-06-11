15d0359d6444fb3ba851f44511f5740bccee72f8
Charly Duque <charly.d.duque@gmail.com>
2024-06-11 14:40:10 -0600

    punto

--- src/components/molecules/hero/hero.styled.js
+++ src/components/molecules/hero/hero.styled.js
@@ -36,7 +36,7 @@ export const StyledDetail = styled.div`
   margin: 1em;
   font-weight: bold;
     @media ${deviceSizes.sm} {
-    width:80%;
+    width:70%;
     margin: 0 auto;
     font-weight: bolder;
   }
